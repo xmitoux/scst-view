3f38c3974a9ccb9944706a1fedc99ae53534882e
mitou <xmitoux@gmail.com>
2020-03-13 16:28:37 +0000

    追加: app bar用共通追加ボタン(+)を作成
    各画面のlayoutでapp barのslotに挿入して使う

---
+++ components/AppBarButttonPlus.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-btn icon @click="$nuxt.$emit(eventName)">
+    <v-icon>mdi-plus</v-icon>
+  </v-btn>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  props: {
+    eventName: {
+      type: String,
+      required: true,
+    },
+  },
+
+  setup(props) {
+    return {
+      eventName: props.eventName,
+    };
+  },
+});
+</script>

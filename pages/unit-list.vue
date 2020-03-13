b3560d2e0c1e94b42ac0d4cbda7aade6a55cc372
mitou <xmitoux@gmail.com>
2020-03-13 13:15:46 +0000

    追加: ログインページへのリダイレクト
    認証確認ミドルウェアによって未認証時のページ閲覧を防ぐ

--- pages/unit-list.vue
+++ pages/unit-list.vue
@@ -4,5 +4,7 @@
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api';
-export default defineComponent({});
+export default defineComponent({
+  middleware: 'authenticated',
+});
 </script>

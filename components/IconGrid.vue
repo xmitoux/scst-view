b455102e8d5f17a439b0661d2a6e1c3ae59517fa
mitou <xmitoux@gmail.com>
2020-03-14 16:57:08 +0000

    追加: 共通アイコングリッドのベースを作成
    とりあえずv-imgでassetの画像を表示させる

---
+++ components/IconGrid.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container>
+    <div v-for="src in srcList" :key="src">
+      <v-img :src="requireSrc(src)" />
+    </div>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  props: {
+    srcList: {
+      type: Array,
+      default: () => [],
+    },
+  },
+
+  setup() {
+    // カスタムコンポーネントにassetを渡すにはrequireが必要
+    const requireSrc = (src: string) => require(`~/assets/icons/${src}`);
+
+    return {
+      requireSrc,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>

dcbe1a089cf02f373ca7ec5efaa6964acdede974
mitou <xmitoux@gmail.com>
2020-03-16 15:30:34 +0000

    変更: ミドルウェアの認証状態取得処理を待機
    状態を取得するまでリダイレクトさせない

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,9 +1,11 @@
 import firebase from '~/plugins/firebase';
 
-export default ({ redirect }) => {
-  firebase.auth().onAuthStateChanged(user => {
-    if (!user) {
-      redirect('/');
-    }
+export default async ({ redirect }) => {
+  const user = await new Promise(resolve => {
+    firebase.auth().onAuthStateChanged(user => resolve(user));
   });
+
+  if (!user) {
+    redirect('/');
+  }
 };
